297715e39f045ad62d8ab202307840ee864685a9
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-12-22 14:19:58 +0300

    Initialization

---
+++ nacar.py
@@ -0,0 +1,6 @@
+walls="x xx xx x"
+sensors=["on","on","off","on"]
+# your code starts
+# your code ends
+print('The most likely current position of the robot is',robot_pos,'with
+probability',robot_pos_prob)
